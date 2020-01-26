8b4bece57f0532c7d7cd97396ebf1ecddd643def
reinketelaars <github.com@reinketelaars.nl>
2020-01-27 00:55:54 +0100

    general improvements, including documentation.

--- frogsay.py
+++ frogsay.py
@@ -12,34 +12,66 @@
 import requests
 import json
 import random
+import sys
 
 FILE_URL = "https://frog.tips/api/1/tips"
 SENTENCE_LENGTH = 50
 
-response = requests.get(FILE_URL)
-dict = json.loads(response.content)
-text = random.choice(list(dict['tips']))['tip']
+class Frog():
+    """The revised and simplified version of frogsay.
+    It retrieves tips from the frog.tips website, 
+    prints an ASCII frog that speaks the random tip.
+    """
+    def __init__(self):
+        pass
 
-phrase = []
-length = 0
-for word in text.split():
-    length += len(word) + 1
-    if length >= SENTENCE_LENGTH:
-        phrase.append("\n       ")
+    def get_tips(self):
+        """Retrieves tips from the site and randomly selects one."""
+        # get data from site
+        response = requests.get(FILE_URL)
+        # get content from site into variable 'dict' as a dictionary
+        dict = json.loads(response.content)
+        # choose a random tip. Only key is 'tips'. Value is entire list of tips.
+        # in the format: list of dictionaries with two keys each ('tip' en 'number')
+        text = random.choice(list(dict['tips']))['tip']
+        # text is a string
+        return text
+
+    def speak(self, text):
+        """Prints an ASCII frog including tha random tip.
+
+        Takes a tip as argument as a string."""
+        phrase = []
         length = 0
-    phrase.append(word)
+        for word in text.split():
+            # Increase length with the length of each word + space.
+            length += len(word) + 1
+            # if length is longer than desired: add a line break
+            if length >= SENTENCE_LENGTH:
+                phrase.append("\n      ")
+                length = 0
+            phrase.append(word)
 
-text = " ".join(phrase)
+        # join the list of words (and line breaks) into a new string.
+        text = " ".join(phrase)
 
-frog = r"""
-
-        {text}
-         /
+        frog = r"""
+       {text}
         /
+       /
   @..@
  (----)
 ( >__< )
-^^ ~~ ^^
-"""
+^^ ~~ ^^ """
 
-print(frog.format(text=text))
+        print(frog.format(text=text))
+        return 0
+
+def main():
+    frog = Frog()
+    text = frog.get_tips()
+    sys.exit(frog.speak(text))
+
+if __name__ == '__main__':
+    main()
+
